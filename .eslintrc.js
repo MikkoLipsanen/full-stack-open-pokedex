ceba8827c88e5eb0fbbfa6eda57df4ce22f280bc
Mikko Lipsanen <lipsanenm@gmail.com>
2025-03-20 22:37:06 +0200

    Tehtävä 11.9 testi 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   'env': {
     'browser': true,
     'es6': true,
-    'jest/globals': true
+    'jest/globals': true,
+    "cypress/globals": true
   },
   'globals': {
     'module': true,
@@ -21,7 +22,7 @@ module.exports = {
     'sourceType': 'module'
   },
   'plugins': [
-    'react', 'jest'
+    'react', 'jest', 'cypress'
   ],
   'rules': {
     'indent': [
