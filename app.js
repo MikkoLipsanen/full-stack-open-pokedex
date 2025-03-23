162459cfca2e839bd0e4e955d88a774fa5b1edf1
Mikko Lipsanen <lipsanenm@gmail.com>
2025-03-23 11:36:21 +0200

    Tehtävä 11.15 tag-testi

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
+// middleware for serving static files
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
@@ -12,6 +13,7 @@ app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
   /* eslint-enable no-console */
 })
+
 // endpoint for health checks, output is 'ok'
 app.get('/health', (_req, res) => {
   res.send('ok')
