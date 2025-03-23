aac32bd74e269f9e63866e52b22fe85a0ef91a22
Mikko Lipsanen <lipsanenm@gmail.com>
2025-03-23 12:51:32 +0200

    Tehtävä 11.16 #skip testi

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-// middleware for serving static files
+// great middleware for serving static files
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
